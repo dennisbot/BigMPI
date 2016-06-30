76e9807c7f2427b03b6aed63209a13c78451472e
Dennis Huillca Portilla <dennisbot@gmail.com>
2016-06-30 14:49:29 -0500

    primer envio de BigMPI

---
+++ src/CMakeFiles/bigmpi.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/collectives_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/collectives_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/fileio_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/fileio_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/neighborhood_collectives_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/neighborhood_collectives_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/reductions_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/reductions_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/rma_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/rma_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/sendrecv_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/sendrecv_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/type_contiguous_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/type_contiguous_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/type_hindexed_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/type_hindexed_x.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/utils.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/utils.c.o"
+  "/home/pavel/Documents/paralelo/BigMPI-master/src/vcollectives_x.c" "/home/pavel/Documents/paralelo/BigMPI-master/src/CMakeFiles/bigmpi.dir/vcollectives_x.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BIGMPI_MAX_INT=1000000"
+  "BIGMPI_VCOLLS_RMA"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/local/mpich/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/bigmpi.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bigmpi.dir/fileio_x.c.o"
+  "CMakeFiles/bigmpi.dir/rma_x.c.o"
+  "CMakeFiles/bigmpi.dir/utils.c.o"
+  "CMakeFiles/bigmpi.dir/reductions_x.c.o"
+  "CMakeFiles/bigmpi.dir/type_hindexed_x.c.o"
+  "CMakeFiles/bigmpi.dir/vcollectives_x.c.o"
+  "CMakeFiles/bigmpi.dir/collectives_x.c.o"
+  "CMakeFiles/bigmpi.dir/sendrecv_x.c.o"
+  "CMakeFiles/bigmpi.dir/neighborhood_collectives_x.c.o"
+  "CMakeFiles/bigmpi.dir/type_contiguous_x.c.o"
+  "libbigmpi.pdb"
+  "libbigmpi.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bigmpi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
